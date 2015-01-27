1945de8156343d6ca80c89e82980baae0f65a0c3
jerrickhoang <jerrickhoang@gmail.com>
2015-01-27 10:11:38 -0500

    initial commit to add PCG topology

---
+++ PSO-new/src/Topology/PCG.java
@@ -0,0 +1,27 @@
+package Topology;
+
+import java.util.Vector;
+
+import PSO.Particle;
+
+public class PCG implements SwarmTopology {
+
+	@Override
+	public boolean isNeighbor(Particle p1, Particle p2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void update(int iteration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vector<Integer> getNeighborsFor(Particle p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
